fdb44c651bad6e2fdac21cfadefe55efc885aee0
sjofre <sjofre@alumnos.inf.utfsm.cl>
2023-04-02 16:27:49 -0400

    feat: create client and server that send and receive messages.

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = '25.72.66.152'  # Dirección IP del servidor
+PORT = 12345  # Puerto en el que escucha el servidor
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        message = input('Escribe un mensaje: ')
+        s.sendall(message.encode())
+        data = s.recv(1024)
+        print('Respuesta del servidor:', data.decode())
---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = '25.72.66.152'  # Dirección IP del servidor
+PORT = 12345  # Puerto en el que escuchará el servidor
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Conexión establecida con', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data)
+            respuesta = input("Ingrese respuesta: ")
+            conn.sendall(respuesta.encode())
